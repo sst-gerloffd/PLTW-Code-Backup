163d592ec55dc112f9c279972cb64b9b5b0790aa
sst-gerloffd <devin.gerloff.03@student.ssttx.org>
2025-10-16 09:48:59 -0500

    Activity 1.2.1 - 10/16/25

--- Lesson 1.2 - Abstraction/GerloffD-Activity1.2.1/GerloffD-Activity1.2.1.py
+++ Lesson 1.2 - Abstraction/GerloffD-Activity1.2.1/GerloffD-Activity1.2.1.py
@@ -2,6 +2,7 @@
 #-----import statements-----
 
 import turtle
+import random as rand                                                           
 
 #-----game configuration----
 
@@ -9,6 +10,7 @@ spot = turtle.Turtle()
 spot_color = "pink"
 spot_shape = "circle" # Possible shapes are: “arrow,” “turtle,” “circle,” “square,” “triangle,” and “classic.” 
 spot_shapesize = 1.5
+score = 0 # Global variable that scores the current user score
 
 #-----initialize turtle-----
 
@@ -19,7 +21,21 @@ spot.shapesize(spot_shapesize)
 #-----game functions--------
 
 def turtle_click(x, y):
-    print(f"Turtle clicked at: ({x}, {y})")
+    change_position()
+
+def change_position():
+    new_x = rand.randint(-400, 400) # Generates new x and y coordinates
+    new_y = rand.randint(-300, 300)
+    
+    spot.penup()
+    spot.goto(new_x,new_y) # Moves to new position.
+    spot.pendown()
+
+
+def update_global_score():
+  global score # This will update the global score.
+  score += 1
+  print(score)
     
 #-----events----------------
 
