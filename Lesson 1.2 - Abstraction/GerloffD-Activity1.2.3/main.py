de2cce83979191da5125ba2a57e80208ac2f0ee8
Devin Gerloff <devin.gerloff.03@student.ssttx.org>
2025-10-27 09:28:54 -0500

    Implement apple falling animation
    Added apple_fall function to animate apple falling.

--- Lesson 1.2 - Abstraction/GerloffD-Activity1.2.3/main.py
+++ Lesson 1.2 - Abstraction/GerloffD-Activity1.2.3/main.py
@@ -6,6 +6,7 @@ apple_image = "pear.gif" # Store the file name of your shape
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
+wn.bgpic("background.gif")
 wn.addshape(apple_image) # Make the screen aware of the new file
 
 apple = trtl.Turtle()
@@ -16,8 +17,22 @@ def draw_apple(active_apple):
   active_apple.shape(apple_image)
   wn.update()
 
+def apple_fall():
+  counter = 0
+  fall_amount = 17
+  new_y = apple.xcor()
+  
+  while counter <= fall_amount:
+    new_y -= counter
+    
+    apple.penup()
+    apple.goto(0,new_y) # Moves to new position.
+    apple.pendown()
+    
+    counter += 1
 
 #-----function calls-----
 draw_apple(apple)
+apple_fall()
 
 wn.mainloop()
