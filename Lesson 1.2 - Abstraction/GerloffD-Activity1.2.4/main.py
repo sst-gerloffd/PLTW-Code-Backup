bd6700687a7df3fdf3eae00d13656877dae2dcab
Devin Gerloff <devin.gerloff.03@student.ssttx.org>
2025-11-05 08:49:59 -0600

    Refactor turtle drawing to create walls with doors

--- Lesson 1.2 - Abstraction/GerloffD-Activity1.2.4/main.py
+++ Lesson 1.2 - Abstraction/GerloffD-Activity1.2.4/main.py
@@ -1,17 +1,30 @@
 import turtle
 
+num_walls = 25
+path_width = 20
+wall_color = "black"
+door_offset = 10
+door_width = path_width
+
 t = turtle.Turtle()
+t.pensize(2)
 t.speed(0)
+t.pencolor(wall_color)
 
 loop_counter = 0
-target = 20  
-step_amount = 10  
+step_amount = path_width
 
-while loop_counter < target:
-    t.forward(step_amount)
+while loop_counter < num_walls:
+    t.forward(door_offset)
+
+    t.penup()
+    t.forward(door_width)
+    t.pendown()
+
+    t.forward(step_amount - door_offset - door_width)
+
     t.right(90)
-    
-    step_amount += 10
+    step_amount += path_width
     loop_counter += 1
 
 turtle.done()
