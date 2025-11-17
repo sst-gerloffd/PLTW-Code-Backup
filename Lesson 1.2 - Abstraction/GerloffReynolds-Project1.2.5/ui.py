cb3f80e75307bbc823227c93aeab705f7117c51a
Devin Gerloff <devin.gerloff.03@student.ssttx.org>
2025-11-17 09:35:25 -0600

    Implement click function to update scores
    Removed placeholder comments and added click function to update scores.

--- Lesson 1.2 - Abstraction/GerloffReynolds-Project1.2.5/ui.py
+++ Lesson 1.2 - Abstraction/GerloffReynolds-Project1.2.5/ui.py
@@ -1,16 +1,6 @@
-'''
-To Do:
-- Add upgrades functionality
-- Add Total Albert & Current Albert score displays
-
-'''
 import turtle
 
-def initialize_ui(screen, font, current_alberts):
-    drawer = turtle.Turtle()
-    drawer.penup()
-    drawer.hideturtle()
-    drawer.goto(200, 200)
-    drawer.write(f"Score: {current_alberts}", align="center", font=font)
-    screen.update()
-    return drawer
+def click(x, y, alberts_per_click, current_alberts, total_alberts):
+    current_alberts += alberts_per_click
+    total_alberts += alberts_per_click
+    return current_alberts, total_alberts
