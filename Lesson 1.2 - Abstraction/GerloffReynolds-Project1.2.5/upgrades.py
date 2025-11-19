b2337e1e7ab70c6e8f4f06e6a8cc6cd256b54e34
Devin Gerloff <devin.gerloff.03@student.ssttx.org>
2025-11-18 19:13:33 -0600

    Refactor upgrade class and instance initialization

--- Lesson 1.2 - Abstraction/GerloffReynolds-Project1.2.5/upgrades.py
+++ Lesson 1.2 - Abstraction/GerloffReynolds-Project1.2.5/upgrades.py
@@ -1,39 +1,30 @@
-# === upgrades.py === #
 class Upgrade:
+
     def __init__(self, name, base_cost, cps_increase=0, click_increase=0):
         self.name = name
         self.level = 0
         self.base_cost = base_cost
-        self.cps_increase = cps_increase      # clicks per second
-        self.click_increase = click_increase  # clicks per click
+        self.cps_increase = cps_increase
+        self.click_increase = click_increase
 
     def cost(self):
-        # Cost increases exponentially with level
         return int(self.base_cost * (1.15 ** self.level))
 
     def purchase(self, current_alberts):
-        if current_alberts >= self.cost():
-            current_alberts -= self.cost()
+        price = self.cost()
+        if current_alberts >= price:
+            current_alberts -= price
             self.level += 1
             return True, current_alberts
         return False, current_alberts
 
-# === Upgrade Instances === #
-mini_albert = Upgrade(
-    name="Mini Albert",
-    base_cost=50,          # Starting cost
-    cps_increase=0.10      # Each mini Albert adds 0.1 CPS
-)
 
-click_upgrade = Upgrade(
-    name="Click Power",
-    base_cost=100,
-    click_increase=1       # Each level increases clicks per click by 1
-)
+mini_albert = Upgrade(name="Mini Albert", base_cost=50, cps_increase=0.10)
+click_upgrade = Upgrade(name="Click Power", base_cost=100, click_increase=1)
 
-# === Helper Functions === #
 def total_cps():
-    return mini_albert.level * mini_albert.cps_increase # Calculates total clicks per second from all upgrades."""
+    return mini_albert.level * mini_albert.cps_increase
+
 
 def total_click_power(base_clicks):
-    return base_clicks + click_upgrade.level * click_upgrade.click_increase # Calculates current clicks per click including upgrades.
+    return base_clicks + click_upgrade.level * click_upgrade.click_increase
